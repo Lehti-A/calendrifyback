1018e665ae4cd32c743924ca20f82313d80acbe1
LEHTI ALVER <lehti.alver@gmail.com>
2025-02-28 14:50:05 +0200

    Initial commit

---
+++ src/main/java/eu/calendrify/calendrifyback/CalendrifybackApplication.java
@@ -0,0 +1,13 @@
+package eu.calendrify.calendrifyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendrifybackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendrifybackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/calendrify/calendrifyback/CalendrifybackApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.calendrify.calendrifyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendrifybackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
